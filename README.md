05d5d5b9389ca3d190aae2b7a5c0ceef97d12164
Usufjon7 <usufkarimzoda14@gmail.com>
2025-08-19 21:20:35 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# projekt2
+# projekt2
+# projekt2
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+    },
+    screens: {
+      "mb": { max: '510px' },
+    }
+  },
+  plugins: [],
+}
+
